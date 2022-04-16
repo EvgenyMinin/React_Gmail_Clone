089f39953260d1704c8ac1a318cd2bc09a4583c1
EvgenyMinin <evgeny.frontend@gmail.com>
2022-04-16 21:01:13 +0300

    add header widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './widgets/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
+      <Header />
+      <h2>Let's build Gmail</h2>
     </div>
   );
 }
---
+++ src/widgets/header/index.tsx
@@ -0,0 +1,43 @@
+import { Avatar, IconButton, Input } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+import styles from "./style.module.css";
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.header__left}>
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png" alt="gmail_logo" />
+            </div>
+
+            <div className={styles.header__middle}>
+                <SearchIcon className={styles.icon} />
+
+                <Input placeholder="Search mail" className={styles.input} />
+
+                <ArrowDropDownIcon className={styles.icon} />
+            </div>
+
+            <div className={styles.header__right}>
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+
+                <Avatar />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
